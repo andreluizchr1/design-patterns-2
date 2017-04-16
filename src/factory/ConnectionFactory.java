093555ee4b4f4ef9b480b458c6ac7a446a299960
André Luiz <andreluizchr1@gmail.com>
2017-04-16 14:40:55 -0300

    Inclusão do getter NomeBanco

--- src/factory/ConnectionFactory.java
+++ src/factory/ConnectionFactory.java
@@ -31,6 +31,7 @@ public class ConnectionFactory {
 	}
 
 	public Connection getConnectionPostgres() {
+
 		try {
 			connection = DriverManager.getConnection("jdbc:postgres://localhost:5432/" + nomeBanco, user, password);
 		} catch (SQLException e) {
@@ -47,6 +48,10 @@ public class ConnectionFactory {
 
 	public String getPassword() {
 		return password;
+	}
+
+	public String getNomeBanco() {
+		return nomeBanco;
 	}
 
 }
