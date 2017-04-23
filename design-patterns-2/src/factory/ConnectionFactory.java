fc6be1424001037002601d0cec7a056bb289576c
André Luiz <andreluizchr1@gmail.com>
2017-04-23 19:21:24 -0300

    Inclusção da Classe ConnectionFactory

---
+++ design-patterns-2/src/factory/ConnectionFactory.java
@@ -0,0 +1,73 @@
+package factory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+	private Connection connection;
+	private String user;
+	private String password;
+	private String nomeBanco;
+
+	public ConnectionFactory(String user, String password, String nomeBanco) {
+		this.user = user;
+		this.password = password;
+		this.nomeBanco = nomeBanco;
+		this.connection = null;
+	}
+
+	/*
+	 * <<<<<<< HEAD Metodo para obtenção de uma conexão mysql =======
+	 * GetConnection do Banco Mysql >>>>>>> padrao_factory
+	 */
+	public Connection getConnectionMysql() {
+
+		try {
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/+" + nomeBanco, user, password);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return connection;
+	}
+
+	/*
+	 * GetConnection do Banco Postgres
+	 */
+	public Connection getConnectionPostgres() {
+
+		try {
+			connection = DriverManager.getConnection("jdbc:postgres://localhost:5432/" + nomeBanco, user, password);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return connection;
+	}
+	
+	/*
+	 * Metodo getUser
+	 */
+	public String getUser() {
+		return user;
+	}
+
+	/*
+	 * Metodo getPassword
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/*
+	 * Metodo NomeBanco
+	 */
+	public String getNomeBanco() {
+		return nomeBanco;
+	}
+
+}
